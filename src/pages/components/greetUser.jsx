b2ab805d728967b3d723e062cac7995cbff8a452
kustr_eedu <eduardokuster1@outlook.com>
2026-09-16 00:34:39 -0300

    v1.1.1b Temporary deprecation of second count on GreetUser.jsx
    Signed-off-by: kustr_eedu <eduardokuster1@outlook.com>

--- src/pages/components/greetUser.jsx
+++ src/pages/components/greetUser.jsx
@@ -11,9 +11,9 @@ const GreetUser = () => {
 
       const hour = String(dateObject.getHours()).padStart(2, '0');
       const minutes = String(dateObject.getMinutes()).padStart(2, '0');
-      const seconds = String(dateObject.getSeconds()).padStart(2, '0');
+      "const seconds = String(dateObject.getSeconds()).padStart(2, '0');"
 
-      const currentTime = hour + ":" + minutes + ":" + seconds;
+      const currentTime = hour + ":" + minutes;
 
       setTime(currentTime);
     }, 1000);
